09756dc6e14c5c91c2e3808f2706aa373f414837
Lee Sutton <leesutton1@gmail.com>
2017-10-05 09:36:45 -0700

    Added hyperlinks on home page to access boards
    Changes to tests.py:
    * Unit tests were created to check that the home view returns
      hyperlinks to the topics pages
    Changes to home.html
    * added hyperlinks to the board topics page

--- boards/tests.py
+++ boards/tests.py
@@ -14,17 +14,24 @@ class HomeTests(TestCase):
     Unit tests for the Home page
     """
 
-    def test_home_view_status(self):
-        """
-        Tests the home view returns a 200 status
-        """
+    def setUp(self):
+        self.board = Board.objects.create(name='Django',
+                                          description='Django board')
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_view_status(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse(
+            'board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response,
+                            'href="{0}"'.format(board_topics_url))
 
 
 class BoardTopicsTests(TestCase):
