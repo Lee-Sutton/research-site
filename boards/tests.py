b2aff352bd8e793669b4d6e43af774ecfdd3d691
Lee Sutton <leesutton1@gmail.com>
2017-11-26 11:57:41 -0800

    Added new topic tests
    * Add unit tests for the new topic sites
    * Next step will be to implement forms
    * Modified html and css to use templates
    
    Signed-off-by: Lee Sutton <leesutton1@gmail.com>

--- boards/tests.py
+++ boards/tests.py
@@ -51,3 +51,33 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+
+class NewTopicsTest(TestCase):
+    """docstring for NewTopicsTest"""
+
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django Board.')
+
+    def test_new_topic_view_success_status_code(self):
+        """
+        Navigating to the new topic should return a status code=200
+        """
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_view_not_found_status_code(self):
+        """
+        If the topic does not exist, a 404 should be returned
+        """
+        url = reverse('new_topic', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_new_topic_url_resolves_new_topic_view(self):
+        """
+        The new topic url should resolve to the new topic view
+        function
+        """
+        self.fail('Need to write this test')
